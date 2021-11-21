25af67425d1ba1a1a10650cd31efd30b197a0185
Cookieso <70982567+Werechang@users.noreply.github.com>
2021-11-21 20:48:36 +0100

    Server socket connection preparations

--- src/main/java/com/gebb/wetell/server/ServerThread.java
+++ src/main/java/com/gebb/wetell/server/ServerThread.java
@@ -1,4 +1,32 @@
 package com.gebb.wetell.server;
 
-public class ServerThread {
+import java.io.*;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+
+    private final Socket clientSocket;
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+
+    protected ServerThread(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+        connect();
+    }
+
+    private void connect() {
+        try {
+            oos = new ObjectOutputStream(new BufferedOutputStream(clientSocket.getOutputStream()));
+            oos.flush();
+            ois = new ObjectInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
--- src/main/java/com/gebb/wetell/server/WeTellServer.java
+++ src/main/java/com/gebb/wetell/server/WeTellServer.java
@@ -1,7 +1,36 @@
 package com.gebb.wetell.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class WeTellServer {
+
+    private ServerSocket serverSocket;
+    private boolean running = true;
+
     public static void main(String[] args) {
         System.out.println("Starting server...");
+        new WeTellServer().start(8000);
+    }
+
+    private void start(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        idle();
+    }
+
+    private void idle() {
+        while (running) {
+            try {
+                Socket socket = serverSocket.accept();
+                new ServerThread(socket).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
