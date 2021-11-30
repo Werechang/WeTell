c45269bd5a9d356211f57e32c178f67d90c8f30b
Ehrenle <83189672+Ehrenle@users.noreply.github.com>
2021-11-30 18:15:52 +0100

    add MessageScene

--- src/test/java/com/gebb/wetell/GUITest.java
+++ src/test/java/com/gebb/wetell/GUITest.java
@@ -2,6 +2,7 @@ package com.gebb.wetell;
 
 import com.gebb.wetell.client.WeTellClient;
 import com.gebb.wetell.client.gui.SceneManager;
+import com.gebb.wetell.client.gui.SceneType;
 import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -15,6 +16,7 @@ public class GUITest extends Application {
         stage.getIcons().add(new Image(Objects.requireNonNull(WeTellClient.class.getResource("gui/icons/wetell.png")).toExternalForm()));
 
         SceneManager sceneManager = new SceneManager(stage, null);
+        sceneManager.setScene(SceneType.MESSAGE);
     }
 
     public static void initialize() {
