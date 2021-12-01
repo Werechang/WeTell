490938212b35694ce4c2dda3755c6cd7c06d3b0a
Cookieso <tom@gottschalck.name>
2021-12-02 00:04:25 +0100

    Documentation changes

--- src/main/java/com/gebb/wetell/client/IGUICallable.java
+++ src/main/java/com/gebb/wetell/client/IGUICallable.java
@@ -1,7 +1,7 @@
 package com.gebb.wetell.client;
 
 /**
- * To easily test the gui and get an overview of methods used by the gui from the IGUICallable.
+ * To easily test the gui and get an overview of methods used by the gui from the IGUICallable in setOnAction().
  */
 public interface IGUICallable {
     void prepareClose();
