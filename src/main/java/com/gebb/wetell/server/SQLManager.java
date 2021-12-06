019f8fed39662f37f402b9253e98260d44f76304
Ehrenle <83189672+Ehrenle@users.noreply.github.com>
2021-12-06 21:03:52 +0100

    Summarising the create methods

--- src/main/java/com/gebb/wetell/server/SQLManager.java
+++ src/main/java/com/gebb/wetell/server/SQLManager.java
@@ -16,6 +16,19 @@ public class SQLManager {
         }
     }
 
+    protected void createTables() {
+        String sql = """
+                CREATE TABLE `users` (`id` int PRIMARY KEY, `name` string, `hashedPassword` string, `salt` string, `profile_pic` string);\s
+                CREATE TABLE `messages` ( `id` int PRIMARY KEY, `sender_id` int FOREIGN KEY, `chat_id` int FOREIGN KEY, `msg_content` string, `send_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP);\s
+                CREATE TABLE `chats` (`id` int PRIMARY KEY, `profile_pic` string, `name` string);\s
+                CREATE TABLE `contacts` (`user_id` int FOREIGN KEY, `chat_id` int FOREIGN KEY);""";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     protected void close() {
         if (conn != null) {
             try {
@@ -26,60 +39,12 @@ public class SQLManager {
         }
     }
 
-    protected void createTables() {
-        createUsersTable();
-        createMessagesTable();
-        createChatsTable();
-        createContactsTable();
-    }
-
     protected void addUser(String username, String password) {
         String sql = "INSERT INTO users(username,password) VALUES(?,?)";
         try {
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setString(1, username);
             stmt.setString(2, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    protected void createUsersTable() {
-        String sql = "CREATE TABLE `users` (`id` int PRIMARY KEY, `name` string, `hashedPassword` string, `salt` string, `profile_pic` string);";
-
-        try {
-            PreparedStatement stmt = conn.prepareStatement(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    protected void createMessagesTable() {
-        String sql = "CREATE TABLE `messages` ( `id` int PRIMARY KEY, `sender_id` int FOREIGN KEY, `chat_id` int FOREIGN KEY, `msg_content` string, `send_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP);";
-
-        try {
-            PreparedStatement stmt = conn.prepareStatement(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    protected void createChatsTable() {
-        String sql = "CREATE TABLE `chats` (`id` int PRIMARY KEY, `profile_pic` string, `name` string);";
-
-        try {
-            PreparedStatement stmt = conn.prepareStatement(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    protected void createContactsTable() {
-        String sql = "CREATE TABLE `contacts` (`user_id` int FOREIGN KEY, `chat_id` int FOREIGN KEY);";
-
-        try {
-            PreparedStatement stmt = conn.prepareStatement(sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
