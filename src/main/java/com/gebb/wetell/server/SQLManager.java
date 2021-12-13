2569e70cb598b27a637568123143676468dc415c
Ehrenle <83189672+Ehrenle@users.noreply.github.com>
2021-12-13 12:20:29 +0100

    SQL System-Prints

--- src/main/java/com/gebb/wetell/server/SQLManager.java
+++ src/main/java/com/gebb/wetell/server/SQLManager.java
@@ -6,7 +6,7 @@ public class SQLManager {
 
     private Connection conn = null;
 
-    protected SQLManager(String path) {
+    protected SQLManager(String path) { //TODO path?
         try {
             // create a connection to the database
             conn = DriverManager.getConnection(path);
@@ -26,41 +26,51 @@ public class SQLManager {
         }
     }
 
+    protected void createDatabase() {
+        try {
+            String sql = "CREATE DATABASE wetell;";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
+            System.out.println("Database successfully created.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     protected void createTables() {
         // users
         try {
             String sql = "CREATE TABLE `users` (`id` int UNSIGNED NOT NULL AUTO_INCREMENT, `name` string, `hashedPassword` string, `salt` String, `profile_pic` string DEFAULT GETSPB(), PRIMARY KEY (id));";
-            PreparedStatement stmt = conn.prepareStatement(sql);
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
+            System.out.println("User table successfully created.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
         // messages
         try {
             String sql = "CREATE TABLE `messages` (`id` int unsigned NOT NULL AUTO_INCREMENT, `sender_id` int, `chat_id` int , `msg_content` string, `send_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id), FOREIGN KEY (sender_id) REFERENCES users(id), FOREIGN KEY (chat_id) REFERENCES chats(id));";
-            PreparedStatement stmt = conn.prepareStatement(sql);
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
+            System.out.println("Messages table successfully created.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
         // chats
         try {
             String sql = "CREATE TABLE `chats` (`id` int unsigned NOT NULL AUTO_INCREMENT, `profile_pic` string, `name` string, PRIMARY KEY (id));";
-            PreparedStatement stmt = conn.prepareStatement(sql);
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
+            System.out.println("Chats table successfully created.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
         // contacts
         try {
             String sql = "CREATE TABLE `contacts` (`user_id` int , `chat_id` int , FOREIGN KEY (user_id) REFERENCES users(id), FOREIGN KEY (chat_id) REFERENCES chats(id));";
-            PreparedStatement stmt = conn.prepareStatement(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    protected void createDatabase() {
-        try {
-            String sql = "CREATE DATABASE wetell;";
-            PreparedStatement stmt = conn.prepareStatement(sql);
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
+            System.out.println("Contacts table successfully created.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -69,10 +79,12 @@ public class SQLManager {
     protected void addUser(String username, String hashedPassword, String salt) {
         String sql = "INSERT INTO users(username,hashedPassword,salt) VALUES(?,?,?)";
         try {
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, username);
-            stmt.setString(2, hashedPassword);
-            stmt.setString(3, salt);
+            PreparedStatement pstmt = conn.prepareStatement(sql);   //TODO Create only if not already available. (Only Username; Password )
+            pstmt.setString(1, username);
+            pstmt.setString(2, hashedPassword);
+            pstmt.setString(3, salt);
+            pstmt.executeUpdate();
+            System.out.println("User successfully added.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -81,12 +93,33 @@ public class SQLManager {
     protected void newMessage(int sender_id, int chat_id, String msg_content){
         String sql = "INSERT INTO messages(sender_id,chat_id,msg_content) VALUES(?,?,?)";
         try {
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setInt(1, sender_id);
-            stmt.setInt(2, chat_id);
-            stmt.setString(3, msg_content);
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, sender_id);
+            pstmt.setInt(2, chat_id);
+            pstmt.setString(3, msg_content);
+            pstmt.executeUpdate();
+            System.out.println("Message successfully added.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    protected void getUser(){
+        String sql = "SELECT ";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);   //TODO Check whether there are users and whether the data entered is correct.
+            pstmt.executeQuery();
+            throw new NullPointerException();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void getMessagesForChat(){
+
+    }
+
+    public static class User {
+
     }
 }
